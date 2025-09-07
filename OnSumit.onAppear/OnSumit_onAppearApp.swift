770ef3f6220e423b5e5ac0e59f726487ce582c12
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-09-07 10:07:54 +0200

    Initial Commit

---
+++ OnSumit.onAppear/OnSumit_onAppearApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnSumit_onAppearApp.swift
+//  OnSumit.onAppear
+//
+//  Created by Авазбек Надырбек уулу on 07.09.25.
+//
+
+import SwiftUI
+
+@main
+struct OnSumit_onAppearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
